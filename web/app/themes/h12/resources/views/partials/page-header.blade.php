7bd3bf0fd19985ec8c7826cdadb6b4e780f1bdfd
Bo Skakke <bo@skakke.dk>
2019-10-10 00:47:14 +0200

    h1

--- web/app/themes/h12/resources/views/partials/page-header.blade.php
+++ web/app/themes/h12/resources/views/partials/page-header.blade.php
@@ -1,3 +1,10 @@
-<div class="page-header">
-  <h1>{!! App::title() !!}</h1>
+@php
+	if ( is_front_page() ) {
+			$tag = 'h2';
+	} else {
+		$tag = 'h1';
+	}
+@endphp
+<div class="page-header h1">
+  <{!! $tag !!} class="h1">{!! App::title() !!}</{!! $tag !!}>
 </div>
