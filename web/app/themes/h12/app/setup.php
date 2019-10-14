0769c8357cc01d1e92c1fe6c19c532db0d743e82
Bo Skakke <bo@skakke.dk>
2019-10-14 13:40:24 +0200

    added old jquery

--- web/app/themes/h12/app/setup.php
+++ web/app/themes/h12/app/setup.php
@@ -16,11 +16,11 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
     if (!is_admin()) {
         // comment out the next two lines to load the local copy of jQuery
-        wp_deregister_script('jquery');
+        // wp_deregister_script('jquery');
         // remove Guthernberg styling
         wp_dequeue_style( 'wp-block-library' );
-        wp_register_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js', false, '3.4.1', true);
-        wp_enqueue_script('jquery');
+        // wp_register_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js', false, '3.4.1', true);
+        // wp_enqueue_script('jquery');
     }
 
 }, 100);
